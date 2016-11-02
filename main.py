ed064d14d4d614a2a83b92fafd338dd58ec68757
Brandon Kurtz <kurtz.brandon@gmail.com>
2016-11-02 16:06:56 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+import os
+import sys
+
+
+def list_files_in(root_dir):
+    return [root_dir + "/" + f for f in os.listdir(root_dir)]
+
+
+def check_length_violations(files, max_len=250):
+    violations = []
+    for f in files:
+        if len(f) >= max_len:
+            violations.append(f)
+    return violations
+
+
+def find_occurences(s, ch):
+    return [i for i, letter in enumerate(s) if letter == ch]
+
+
+def check_illegal_chars(root, directories, illegal_characters,
+                        replacement_char='-', replace=False):
+    violations = []
+    for directory in directories:
+        for character in illegal_characters:
+            for i in find_occurences(directory, character):
+                if replace:
+                    new_dir = directory[:i] + replacement_char + directory[i+1:]  # noqa
+                    os.rename(directory, new_dir)
+                    violations = check_illegal_chars(root,
+                                                     directories,
+                                                     illegal_characters)
+                else:
+                    violations.append(directory)
+
+    return violations
+
+
+def create_compliance_log(root,
+                          illegal_chars,
+                          replace_char='-',
+                          max_len=250,
+                          replace=False,
+                          log_path='compliance_log.txt'):
+
+    # list of all files in $root with full path
+    files = list_files_in(root)
+    # list of all files in $root with illegal characters
+    directories_with_illegal_chars = check_illegal_chars(root,
+                                                         files,
+                                                         illegal_chars,
+                                                         replacement_char=replace_char,  # noqa
+                                                         replace=replace)
+    # list of all files in $root that are too long
+    directories_with_illegal_length = check_length_violations(files,
+                                                              max_len=max_len)
+
+    print(directories_with_illegal_length)
+
+    f = open(log_path, 'w')
+    for d in directories_with_illegal_chars:
+        f.write('illegal char: ' + d + '\n')
+    f.write('\n'*2)
+    for d in directories_with_illegal_length:
+        f.write('!!Greater than ' + str(max_len) + ": " + str(d) + '\n')
+
+    f.close()
+
+
+def main():
+    root = '/Users/Brandon/Desktop/file_compliance/testing'
+    illegal_chars = ['$', '%', '^', '*', ' ']
+
+    create_compliance_log(root, illegal_chars, replace=True)
+
+main()
